a7020fbe13f3ddb39777eea59065caa4834d3c44
hophacker <jiefeng.hopkins@gmail.com>
2019-06-05 12:20:10 +0800

    remove jwt.dispatch_requests, and jwt.revocation_requests in setup of devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -298,12 +298,6 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.dispatch_requests = [
-      ['POST', %r{^/users/sign_in$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/users/sign_out$}]
-    ]
     jwt.expiration_time = 1.day.to_i
   end
   config.navigational_formats = []
