834507b88d3e696191262d9214fecc3ed400333b
wsq <wsq@rallets.com>
2019-06-18 19:45:07 +0800

    delete device type

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -3,13 +3,12 @@ class JWTBlacklist < ApplicationRecord
 
   def self.jwt_revoked?(payload, user)
     # Check if in the blacklist
-    $redis.get("user_blacklist:#{user.id}:#{payload['device']}:#{payload['jti']}").present?
+    $redis.get("user_blacklist:#{user.id}:#{payload['jti']}").present?
   end
 
   def self.revoke_jwt(payload, user)
     # REVOKE JWT
     expiration = payload['exp'] - payload['iat']
-    $redis.setex("user_blacklist:#{user.id}:#{payload['device']}:#{payload['jti']}", expiration, payload['jti'])
-    $redis.del("user_device_jwt:#{user.id}:#{payload['device']}")
+    $redis.setex("user_blacklist:#{user.id}:#{payload['jti']}", expiration, payload['jti'])
   end
 end
