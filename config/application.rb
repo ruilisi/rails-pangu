abdcf799181b68ec1e7fae20b015f854b744d019
jiaweili <c@rallets.com>
2019-06-19 12:39:21 +0800

    allow put and delete

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module RailsDeviseJWT
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
       end
     end
     # Initialize configuration defaults for originally generated Rails version.
