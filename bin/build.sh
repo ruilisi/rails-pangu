6f8ef75187d71cc0bc989a19687db57df862018a
hophacker <j@paiyou.co>
2019-08-14 11:14:20 +0800

    accelerates ruby gem bundling for ruby users in China

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker run  -v $(pwd):/usr/src/app/ -it --rm rails-pangu bundle install
+docker run  -v $(pwd):/usr/src/app/ -it --rm ruby sh -c 'cd /usr/src/app/
+bundle config mirror.https://rubygems.org https://gems.ruby-china.com
+bundle install'
 docker build -t rails-pangu -f Dockerfile .
