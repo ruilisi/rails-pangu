b9ef301039ca7918d4e06a369c2c62022ae1e00f
hophacker <j@paiyou.co>
2019-10-28 17:54:12 +0800

    code clean

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -4,12 +4,10 @@ class JWTBlacklist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Blacklist
 
   def self.jwt_revoked?(payload, user)
-    # Check if in the blacklist
-    $redis.get("user_blacklist:#{user.id}:#{payload['jti']}").present?
+    $redis.exists("user_blacklist:#{user.id}:#{payload['jti']}").present?
   end
 
   def self.revoke_jwt(payload, user)
-    # REVOKE JWT
     expiration = payload['exp'] - payload['iat']
     $redis.setex("user_blacklist:#{user.id}:#{payload['jti']}", expiration, payload['jti'])
   end
