4878bd87f36cbe3f9005496b1567abd0140101d0
hophacker <j@paiyou.co>
2019-11-23 17:38:37 +0800

    add configuration for Rack::Cors which allows requests from any origin, also exposed 'Authorization'

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ Bundler.require(*Rails.groups)
 
 module RailsPangu
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options put delete], expose: ['Authorization']
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
