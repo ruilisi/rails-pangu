d14d55171c7077c337c4e29aa418d8f3482bb8d6
hophacker <j@paiyou.co>
2019-12-07 14:25:38 +0800

    add support for jwt authentication through action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,21 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+    def find_verified_user
+      token = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(',').last.strip
+      if (current_user = UtilJwt.user_from_jwt_token(token))
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
