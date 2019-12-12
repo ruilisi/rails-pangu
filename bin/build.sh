6c110d452f47d58333ec1d775d8309e666a5e938
hophacker <j@paiyou.co>
2019-12-12 16:48:03 +0800

    bundle Gemfile with ruby:2.6.5

--- bin/build.sh
+++ bin/build.sh
@@ -12,7 +12,7 @@ while getopts ":B" opt; do
 done
 
 if [[ $BUNDLE == 'true' ]]; then
-  docker run  -v $(pwd):/usr/src/app/ -it --rm ruby sh -c 'cd /usr/src/app/
+  docker run  -v $(pwd):/usr/src/app/ -it --rm ruby:2.6.5 sh -c 'cd /usr/src/app/
   bundle config mirror.https://rubygems.org https://gems.ruby-china.com
   bundle install'
 fi
