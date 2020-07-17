68c5a6a360ef90474c8e736cefbe4cbc378ff88d
hophacker <j@paiyou.co>
2020-07-17 15:16:51 +0800

    upgrade libraries

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class JwtBlacklist < ApplicationRecord
-  include Devise::JWT::RevocationStrategies::Blacklist
+  include Devise::JWT::RevocationStrategies::Denylist
 
   def self.jwt_revoked?(payload, user)
     $redis.exists("user_blacklist:#{user.id}:#{payload['jti']}").present?
