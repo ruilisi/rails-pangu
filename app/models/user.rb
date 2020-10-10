b776e819044fafc70cfa3c8efd2f2957e0e7313f
hophacker <j@paiyou.co>
2020-10-10 10:54:27 +0800

    add jwt_denylist, apply it for devise-jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
---
+++ config/initializers/jwt_denylist.rb
@@ -0,0 +1,12 @@
+class JwtDenylist < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::Denylist
+
+  def self.jwt_revoked?(payload, user)
+    $redis.exists?("user_denylist:#{user.id}:#{payload['jti']}")
+  end
+
+  def self.revoke_jwt(payload, user)
+    expiration = payload['exp'] - payload['iat']
+    $redis.setex("user_denylist:#{user.id}:#{payload['jti']}", expiration, payload['jti'])
+  end
+end
